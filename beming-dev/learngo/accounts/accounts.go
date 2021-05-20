d94f4291cc50e0a76504e7b6f0546e50548748e9
beming-dev <mingfordev@gmail.com>
2021-05-20 17:08:31 +0900

    Add method

--- beming-dev/learngo/accounts/accounts.go
+++ beming-dev/learngo/accounts/accounts.go
@@ -11,3 +11,18 @@ func NewAccount(owner string) *Account {
 	account := Account{owner: owner, balance: 0}
 	return &account
 }
+
+//receiver = lowercase of structure
+//Deposit x amount on your account
+func (a *Account) Deposit(amount int) {
+	a.balance += amount
+}
+
+//Balance of your account
+func (a *Account) Balance() int {
+	return a.balance
+}
+
+func (a *Account) Owner() string {
+	return a.owner
+}
